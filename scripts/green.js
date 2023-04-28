c2bfff3b7de76af8eef65e63ce153ee6312de263
aivct <79041835+aivct@users.noreply.github.com>
2023-04-28 04:08:57 -0400

    infinite loop bodge

--- scripts/green.js
+++ scripts/green.js
@@ -6,7 +6,27 @@
 // grey also works: #D3EEDD
 const GREEN_COLOUR_HEX = "#6B9797"; // trying to pick a relaxing color that fades into the background while still being readable
 
-// stupidly brittle method, but eh, it works.
-let notification = document.querySelector(".yt-spec-icon-badge-shape__badge");
-// cuz sometimes it's null 
-if(notification) notification.style.background = GREEN_COLOUR_HEX;
+const TIMEOUT = 1000;
+// infinite loop method, stupid but it works.
+setTimeout(checkNotification, TIMEOUT)
+function checkNotification()
+{
+	let notification = document.querySelector(".yt-spec-icon-badge-shape__badge");
+	if(notification)
+	{
+		// end if notification found.
+		changeNotificationColour(GREEN_COLOUR_HEX);
+	}
+	else 
+	{
+		setTimeout(checkNotification, TIMEOUT);
+	}
+}
+
+function changeNotificationColour(colour)
+{
+	// stupidly brittle method, but eh, it works.
+	let notification = document.querySelector(".yt-spec-icon-badge-shape__badge");
+	// cuz sometimes it's null 
+	if(notification) notification.style.background = colour;
+}
